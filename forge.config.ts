ff006b5ff3f1c2ed166e9b10699ba2dd72787ea6
Peter Williams <Peter.Williams@wisetechglobal.com>
2026-03-18 09:04:52 +1100

    Bump version to 1.0.4

--- forge.config.ts
+++ forge.config.ts
@@ -13,7 +13,7 @@ const config: ForgeConfig = {
     asar: true,
     name: 'Markover',
     executableName: 'Markover',
-    appVersion: '1.0.3',
+    appVersion: '1.0.4',
     appCopyright: `Copyright © ${new Date().getFullYear()} Peter Williams`,
   },
   publishers: [
